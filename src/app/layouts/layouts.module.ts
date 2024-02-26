9ed6087471d43d9d4e99389dce9f5e0362465b10
romirahman <romirahman03romi@gmail.com>
2024-02-26 20:51:38 +0700

    new: structure project

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutsComponent } from './layouts.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule { }
